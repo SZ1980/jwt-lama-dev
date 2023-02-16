8d7cfa2c4c7fa41e3ab8646dfe17d42a4a26462c
Zoran Simonovic <simonovic1980@gmail.com>
2023-02-16 08:59:41 +0100

    starting point of development

--- api/index.js
+++ api/index.js
@@ -1,1 +1,27 @@
-console.log("start");
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const users = [
+  {
+    id: "1",
+    username: "john",
+    password: "John0908",
+    isAdmin: true,
+  },
+  {
+    id: "2",
+    username: "jane",
+    password: "Jane0908",
+    isAdmin: false,
+  },
+];
+
+app.post("/api/login", (req, res) => {
+  // const { username, password } = req.body;
+  res.send("Hey it is working");
+});
+
+app.listen(5000, () => {
+  console.log("Backend server is running on port 5000");
+});
